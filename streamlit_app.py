4ac4ddc2b10c4e25a69e50d19dc03174c6f431b9
subbuveluri <127949235+subbuveluri@users.noreply.github.com>
2023-03-15 17:58:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ streamlit.text('Avocado Toast')
 streamlit.header('Bulid your own fruit smoothie')
 
 import pandas
-my_fruit_list=pandas.read_csv("https;//uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
