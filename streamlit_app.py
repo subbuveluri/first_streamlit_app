91107ee21e4a548a84993b57f8de4637490bca04
subbuveluri <127949235+subbuveluri@users.noreply.github.com>
2023-03-15 17:24:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast menu')
+
+streamlit.text('omega 3 & bluberry oatmeal')
+
+streamlit.text('kale, spinach & rocket smoothie')
+
+streamlit.text('Hard-boiled free-range egg')
