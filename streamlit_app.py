204f07010b9d3b0a8467f495ac17df99ade4692f
subbuveluri <127949235+subbuveluri@users.noreply.github.com>
2023-03-15 17:57:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,7 @@ streamlit.text('Hard-boiled free-range egg')
 streamlit.text('Avocado Toast')
 
 streamlit.header('Bulid your own fruit smoothie')
+
+import pandas
+my_fruit_list=pandas.read_csv("https;//uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
