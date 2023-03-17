848f606e84d53e01c8a668beefab041f60870caa
subbuveluri <127949235+subbuveluri@users.noreply.github.com>
2023-03-17 11:29:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,6 @@ streamlit.header('Bulid your own fruit smoothie')
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','straw berries'])
-fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
